d26547c2f1102f4db0e5143af118e7c7282ada44
KyleZ <kylezhang1118@gmail.com>
2021-06-11 18:20:54 +0800

    A mistake in FastMNMF2 has been revised in line 104. Thanks to "qiansichong".

--- command.m
+++ command.m
@@ -1,6 +1,6 @@
 clc;clear;
 close all;
-Angle = [90,315];                               %Angle,Options:90,135,180,270 plus:0,45,225,315
+Angle = [45,135];                               %Angle,Options:90,135,180,270 plus:0,45,225,315
 if(exist('SetupStruc','var'))
     [s,sOri,Unmix_s,~] = readData(Angle,ISM_setup);
 else
