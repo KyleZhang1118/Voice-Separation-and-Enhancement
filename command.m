b2df50f057c630ef421c2c9e3a885195cadef8ba
Ke <kylezhang1118@gmail.com>
2021-11-01 18:11:04 +0800

    Signed-off-by: Ke <kylezhang1118@gmail.com>

--- command.m
+++ command.m
@@ -6,7 +6,7 @@ if(exist('SetupStruc','var'))
 else
     [s,sOri,Unmix_s,SetupStruc] = readData(Angle,ISM_setup);    % 's' is the muli-channel mixture signal,size:Length*7,
 end                                            % 'sOri' is the each single singal of the 1st channel corresponding to the angle,size:Length*N
-method = {'DSB'                0;
+method = {'DSB'                1;
           'DSB_Mask'           0;
           'MVDR'               0;
           'MVDR_ESB'           0;
@@ -21,7 +21,7 @@ method = {'DSB'                0;
           'ICA_funda'          0;
           'ICA_initial'        0;
           'ICA_Sawada'         0;
-          'FastICA_HO_Sawada'  1;
+          'FastICA_HO_Sawada'  0;
           'IVA'                0;
           'IVA_woDR'           0;
           'AuxIVA'             0;
